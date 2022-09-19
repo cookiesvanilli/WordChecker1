4e1eb65ad3551bd6674e482877a0a47b52b4b26b
Inna K <kuklina.inn@yandex.ru>
2022-09-19 20:31:33 +0300

    change wordChecker

--- src/WordChecker.java
+++ src/WordChecker.java
@@ -1,34 +1,18 @@
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class WordChecker {
     protected String text;
+    Set<String> set = new HashSet<>();
 
     public WordChecker(String text) {
         this.text = text;
+        String[] item = text.split("\\P{IsAlphabetic}+");
+        Collections.addAll(set, item);
     }
 
     public boolean hasWord(String word) {
-        HashMap<String, Integer> map = new HashMap<>();
-
-        String[] item = text.split("\\P{IsAlphabetic}+");
-        for (String t : item) {
-            if (map.containsKey(t)) {
-                map.put(t, map.get(t) + 1);
-
-            } else {
-                map.put(t, 1);
-            }
-        }
-
-        Set<String> keys = map.keySet();
-        for (String key : keys) {
-            if (key.contains(word)) {
-                return true;
-            }
-
-        }
-        return false;
-
+        return set.contains(word);
     }
 }
